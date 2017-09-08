4f0394e6d6a0e79ddd886238b69ec98e3ebe496c
tkchang9 <tylerkchang@gmail.com>
2017-09-08 00:48:35 -0400

    js fade
    javascript fade effects on load

---
+++ index.js
@@ -0,0 +1,10 @@
+function fadeEffect(targetId) {
+  var ftarget = document.getElementById(targetId);
+  ftarget.style.transition = "opacity 1.7s";
+  ftarget.style.opacity = 1
+}
+
+window.onload = function() {
+  fadeEffect("leftBar");
+  setTimeout(function() {fadeEffect("rightContent")}, 600);
+}
