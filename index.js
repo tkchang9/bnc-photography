56ff58c552379710c8ed39d2cda8536fd80e4f2c
tkchang9 <tylerkchang@gmail.com>
2017-09-11 03:08:55 -0400

    archive changes
    added slideshow to archive page

--- index.js
+++ index.js
@@ -4,7 +4,30 @@ function fadeEffect(targetId) {
   ftarget.style.opacity = 1
 }
 
+var slideIndex = 1;
+function moveSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("ssWrap");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className +=" active";
+}
 window.onload = function() {
   //fadeEffect("leftBar");
   setTimeout(function() {fadeEffect("rightContent")}, 300);
+  showSlides(slideIndex);
 }
